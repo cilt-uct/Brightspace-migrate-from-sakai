cilt-uct/Brightspace-migrate-from-sakai

commit 8ff74528ee629a75e5206c4291d0d978dafb5f03

Updated middleware calls to include authentication.

work/push_rubrics.py
@@ -24,6 +24,12 @@ from lib.local_auth import *
 
 def run(SITE_ID, APP, import_id, transfer_id, title, now_st = None):
 
+    tmp = getAuth(APP['auth']['middleware'])
+    if (tmp is not None):
+        AUTH = {'host' : tmp[0], 'user': tmp[1], 'password': tmp[2]}
+    else:
+        raise Exception("Middleware Authentication required")
+
     logging.info(f'Push rubrics to orgunitid {import_id} coursecode {transfer_id} title "{title}"')
 
     # Rubrics XML file
@@ -87,7 +93,7 @@ def run(SITE_ID, APP, import_id, transfer_id, title, now_st = None):
         payload = {'org_id': import_id}
         files = [('file', (file_name, zip_file, 'application/zip'))]
         response = requests.post("{}{}".format(APP['middleware']['base_url'], APP['middleware']['import_url']),
-                                 data=payload, files=files)
+                                 data=payload, files=files, auth=(AUTH['user'], AUTH['password'])
         response.raise_for_status()
     else:
         logging.warning("No rubrics zip file created, nothing to do")

check_imported.py
@@ -13,7 +13,6 @@ import logging
 import requests
 import json
 import importlib
-import requests
 import re
 
 from requests.exceptions import HTTPError
@@ -196,23 +195,13 @@ def check_for_amathuba_id(search_site_id):
     # it's not a workflow failure, we just retry again later.
 
     try:
-        endpoint = "{}{}".format(APP['middleware']['base_url'], APP['middleware']['search_url'])
-        json_response = '';
-
-        response = requests.post(endpoint, data={'type':'code', 'val': search_site_id})
-
-        if not response.ok:
-            logging.warning(f"Response {response.status_code} from {endpoint} searching for {search_site_id}")
+        endpoint = "{}{}?code={}".format(APP['middleware']['base_url'], APP['middleware']['search_url'], search_site_id)
+        json_response = middleware_api(APP, endpoint)
+        if 'status' in json_response:
+            if (json_response['status'] == 'success'):
+                return int(json_response['data']['Identifier'])
         else:
-            json_response = response.json()
-            if 'status' in json_response:
-                # print(json_response)
-                if (json_response['status'] == 'success'):
-                    # print(json_response['data']['Items'])
-                    if len(json_response['data']['Items']) > 0:
-                        return int(json_response['data']['Items'][0]['Identifier'])
-            else:
-                logging.warning(f"Unexpected response {json_response} checking for {search_site_id}")
+            logging.warning(f"Unexpected response {json_response} checking for {search_site_id}")
 
     except Exception as err:
         logging.warning(f"Unexpected {err} in fetch_course_info for {search_site_id}")

config/config.py
@@ -28,7 +28,9 @@ APP = {
     'sakai': 'Vula',
     'sakai_archive' : 'VulaArchive',
     'sakai_db': 'VulaDb',
-    'middleware': 'BrightspaceMiddleware'
+    'middleware': 'BrightspaceMiddleware',
+    'webAuth': 'BrightspaceWeb',
+    'webDAV': 'BrightspaceWebdav'
   },
 
   # In Brightspace the imported site will get this prefix added to their title

lib/utils.py
@@ -26,6 +26,7 @@ parent = os.path.dirname(current)
 sys.path.append(parent)
 
 from lib.jira_rest import MyJira
+from lib.local_auth import getAuth
 
 class myFile(object):
     def __init__(self, filename):
@@ -366,6 +367,12 @@ def stripspace(orig):
 # Call middleware API and return JSON response with optional retries
 def middleware_api(APP, url, payload_data = None, retries = None, retry_delay = None):
 
+    tmp = getAuth(APP['auth']['middleware'])
+    if (tmp is not None):
+        AUTH = {'host' : tmp[0], 'user': tmp[1], 'password': tmp[2]}
+    else:
+        raise Exception("Middleware Authentication required")
+
     if retries is None:
         retries = APP['middleware']['retries']
 
@@ -382,11 +389,15 @@ def middleware_api(APP, url, payload_data = None, retries = None, retry_delay = 
 
         try:
             if payload_data is not None:
-                response = requests.post(url, data=payload_data)
+                response = requests.post(url, data=payload_data, auth=(AUTH['user'], AUTH['password']))
             else:
-                response = requests.get(url)
+                response = requests.get(url, auth=(AUTH['user'], AUTH['password']))
 
             last_status = response.status_code
+
+            if last_status == 401:
+                logging.error(f"API call {url} is Unauthorized")
+                return {'status': 'ERR', 'data': 'Unauthorized'}
 
             if (last_status < 500) and '{' in response.text and '}' in response.text:
                 # Succeeded

work/push_content.py
@@ -23,6 +23,12 @@ from lib.local_auth import *
 
 def run(SITE_ID, APP, import_id, transfer_id, title, now_st = None):
 
+    tmp = getAuth(APP['auth']['middleware'])
+    if (tmp is not None):
+        AUTH = {'host' : tmp[0], 'user': tmp[1], 'password': tmp[2]}
+    else:
+        raise Exception("Middleware Authentication required")
+
     logging.info(f'Push content to orgunitid {import_id} coursecode {transfer_id} title "{title}"')
 
     content_folder = r'{}{}-content/'.format(APP['output'], SITE_ID)
@@ -67,7 +73,7 @@ def run(SITE_ID, APP, import_id, transfer_id, title, now_st = None):
         payload = {'org_id': import_id}
         files = [('file', (file_name, zip_file, 'application/zip'))]
         response = requests.post("{}{}".format(APP['middleware']['base_url'], APP['middleware']['import_url']),
-                                 data=payload, files=files)
+                                 data=payload, files=files, auth=(AUTH['user'], AUTH['password']))
         response.raise_for_status()
 
     return

commit c954225582ab439827681bc16ace8e021a5bc5f1

Fix mismatched brackets
Regression from 8ff74528ee629a75e5206c4291d0d978dafb5f03

work/push_rubrics.py
@@ -93,7 +93,7 @@ def run(SITE_ID, APP, import_id, transfer_id, title, now_st = None):
         payload = {'org_id': import_id}
         files = [('file', (file_name, zip_file, 'application/zip'))]
         response = requests.post("{}{}".format(APP['middleware']['base_url'], APP['middleware']['import_url']),
-                                 data=payload, files=files, auth=(AUTH['user'], AUTH['password'])
+                                 data=payload, files=files, auth=(AUTH['user'], AUTH['password']))
         response.raise_for_status()
     else:
         logging.warning("No rubrics zip file created, nothing to do")