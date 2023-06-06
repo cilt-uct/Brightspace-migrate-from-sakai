947ac7e7b27248ae42fe0eb7a676f319de1b3125
Loyiso <loyison@users.noreply.github.com>
2023-06-06 15:51:51 +0200

    AMA-635-replace-vula-email-with-cilthelpdesk
    update, create method function for key,value change text

--- work/lessonbuilder_replace_vula_email.py
+++ work/lessonbuilder_replace_vula_email.py
@@ -17,53 +17,50 @@ from lib.utils import *
 
 current = os.path.dirname(os.path.realpath(__file__))
 
+# string replace key,val
+chars_to_replace = {'help@vula.uct.ac.za': 'cilt-helpdesk.uct.ac.za', 'The Vula Help Team': 'CILT Help Desk', 'Vula Help': 'CILT Help Desk'}
+
+def replace_with_text(html, char_to_replace):
+    for element in html.find_all(string=True):
+        for key, value in char_to_replace.items():
+            if key in element:
+                new_text = element.replace(key, value)
+                element.replace_with(new_text)
+                return BeautifulSoup('html', 'html.parser')
+  
+  
 def run(SITE_ID, APP):
-  logging.info('Lessons: Replace help@vula.uct.ac.za with cilt-helpdesk@uct.ac.za : {}'.format(SITE_ID))
+  logging.info('Lessons: Replace vula email and team with cilt helpdesk name and team: {}'.format(SITE_ID))
 
-  xml_src = r'{}{}-archive/lessonbuilder.xml'.format(APP['archive_folder'], SITE_ID)
-  xml_old = r'{}{}-archive/lessonbuilder.old'.format(APP['archive_folder'], SITE_ID)
-  shutil.copyfile(xml_src, xml_old)
+xml_src = r'{}{}-archive/lessonbuilder.xml'.format(APP['archive_folder'], SITE_ID)
+xml_old = r'{}{}-archive/lessonbuilder.old'.format(APP['archive_folder'], SITE_ID)
+shutil.copyfile(xml_src, xml_old)
 
-  tree = ET.parse(xml_src)
-  root = tree.getroot()
-
+tree = ET.parse(xml_src)
+root = tree.getroot()
+def run():
   if root.tag == 'archive':
     for item in root.findall(".//item[@type='5']"):
               # pass the html here
               html = BeautifulSoup(item.attrib['html'], 'html.parser')
-              # replace all occurences of the emails, pass as vars
-              oldmail = 'help@vula.uct.ac.za'
-              newmail = 'cilt-helpdesk@uct.ac.za'
-              vulateam = 'The Vula Help Team'
-              vulahelp = 'Vula Help'
-              cilthelp = 'CILT Help Desk'
+              replace_with_text(html, chars_to_replace)
               
-              def update_email(node):
-                  if node.parent and node.parent.name == 'a':
-                      node.parent['href'] = node.parent['href'].replace(oldmail, newmail)
-                      node.replace_with(newmail)
-                  else:
-                      node.replace_with(str(node).replace(oldmail, newmail).replace(vulahelp, cilthelp).replace(vulateam, cilthelp))
-
-              text_nodes = html.find_all(string=True)
-              for node in text_nodes:
-                  update_email(node)
-
               item.set('html', str(html))
 
               tree.write(xml_src, encoding='utf-8', xml_declaration=True)
 
-def main():
-    global APP
-    parser = argparse.ArgumentParser(description="Replace help@vula.uct.ac.za with cilt-helpdesk@uct.ac.za",
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("SITE_ID", help="The SITE_ID on which to work")
-    parser.add_argument('-d', '--debug', action='store_true')
-    args = vars(parser.parse_args())
+  def main():
+      global APP
+      parser = argparse.ArgumentParser(description="Replace vula email and team with cilt helpdesk name and team",
+                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+      parser.add_argument("SITE_ID", help="The SITE_ID on which to work")
+      parser.add_argument('-d', '--debug', action='store_true')
+      args = vars(parser.parse_args())
 
-    APP['debug'] = APP['debug'] or args['debug']
+      APP['debug'] = APP['debug'] or args['debug']
 
-    run(args['SITE_ID'], APP)
+      run(args['SITE_ID'], APP)
 
-if __name__ == '__main__':
-    main()
+  if __name__ == '__main__':
+      main()
+
