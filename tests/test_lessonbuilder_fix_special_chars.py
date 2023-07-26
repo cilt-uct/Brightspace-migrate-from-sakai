aa42ce45648f56695ecd3dedc123a0dc5b6d3cd4
Loyiso <loyison@users.noreply.github.com>
2023-07-26 13:30:20 +0200

    AMA-651-files-folders-remove-colon-characte-test
    WIP: rewrite url special char fix

--- tests/test_lessonbuilder_fix_special_chars.py
+++ tests/test_lessonbuilder_fix_special_chars.py
@@ -2,39 +2,42 @@ import os
 import shutil
 import unittest
 import argparse
-import config
+
 import config.config
-from work.lessonbuilder_rewrite_urls import fix_unwanted_url_chars
+from work.lessonbuilder_rewrite_urls import main
 from unittest.mock import patch
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
 
 class LessonbuilderUpdateUrlRewriteTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        config.config.APP['archive_folder'] = self.ROOT_DIR + '/test_files/'
 
     @patch('os.rename')
     @patch('argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(SITE_ID='123456', debug=True))
 
     def test_url_rewrite_chars_fix(self, *_):
+        main()
         xml_src = self.ROOT_DIR + '/test_files/site_123456-archive/lessonbuilder.xml'
         xml_old = self.ROOT_DIR + '/test_files/site_123456-archive/lessonbuilder.old'
         self.check_urls_chars(xml_src=xml_src)
         shutil.move(xml_old, xml_src)
-    def check_urls_chars(self, xml_src: str):
 
+    def check_urls_chars(self, xml_src: str):
         tree = ET.parse(xml_src)
         root = tree.getroot()
+        pages = root.findall(".//page")
 
-        urlprefix = 'https://vula.uct.ac.za/access/content/group/site_123456'
-        expected_outcome = '../Lesson 1 with in title/Jpeg_thumb_artifacts_test.jpg'
+        # check xml_src is updated here
+        with open(xml_src, 'r') as f:
+            data = f.read()
+        print(data)
 
-        for item in root.findall(".//item[@type='5']"):
-            html = BeautifulSoup(item.attrib['html'], 'html.parser')
-            for element in html.find_all(attrs={'src': True}):
-                current_url = element.get('src')
-                self.assertEquals((fix_unwanted_url_chars(current_url, urlprefix)), expected_outcome)
+        for page in pages:
+            items = page.findall(".//item[@type='5']")
+            for item in items:
+                print(item)
 
 if __name__ == '__main__':
     unittest.main(failfast=True)
