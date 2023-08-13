ebf22248f84094459967b9fafa63aa9a06324834
NgoniChoga <nickchoga@gmail.com>
2023-08-13 18:47:10 +0100

    AMA-726 - highlight links in a tag

--- work/lessonbuilder_highlight_external_links.py
+++ work/lessonbuilder_highlight_external_links.py
@@ -22,8 +22,10 @@ from lib.utils import *
 def run(SITE_ID, APP):
     logging.info('Highlight Sakai tools : {}'.format(SITE_ID))
 
-    xml_src = r'{}{}-archive/lessonbuilder.xml'.format(APP['archive_folder'], SITE_ID)
-    xml_old = r'{}{}-archive/lessonbuilder.old'.format(APP['archive_folder'], SITE_ID)
+    # xml_src = r'{}{}-archive/lessonbuilder.xml'.format(APP['archive_folder'], SITE_ID)
+    # xml_old = r'{}{}-archive/lessonbuilder.old'.format(APP['archive_folder'], SITE_ID)
+    xml_src = 'lessonbuilder.xml'
+    xml_old = 'lessonbuilder.old'
     shutil.copyfile(xml_src, xml_old)
 
     remove_unwanted_characters(xml_src)
@@ -46,28 +48,36 @@ def run(SITE_ID, APP):
             for link in APP['lessons']['lessons_highlight_domains']:
                 pattern = re.compile(f'{link}(/\S+)?', re.IGNORECASE)
                 occurrences = html.find_all(string=pattern)
+
+                occurrences_links = html.find_all('a')
+                for occurrences_link in occurrences_links:
+                    if link in str(occurrences_link):
+                        occurrences_link['style'] = 'color: red; font-weight: bold;'
+
                 for rep in occurrences:
                     replacement = r'<span style="color: red; font-weight: bold;" data-type="link">{}</span>'.format(link)
                     highlighted = pattern.sub(replacement, rep)
                     highlighted_html = BeautifulSoup(highlighted, 'html.parser')
                     rep.replace_with(highlighted_html)
-                    item.set('html', str(html))
+
+                item.set('html', str(html))
 
         tree.write(xml_src, encoding='utf-8', xml_declaration=True)
 
 
 def main():
     global APP
-    parser = argparse.ArgumentParser(
-        description="This script takes as input the 'lessonbuilder.xml' file inside the site-archive folder and replaces adds a class to the ol and removes styling on li",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("SITE_ID", help="The SITE_ID on which to work")
-    parser.add_argument('-d', '--debug', action='store_true')
-    args = vars(parser.parse_args())
+    # parser = argparse.ArgumentParser(
+    #     description="This script takes as input the 'lessonbuilder.xml' file inside the site-archive folder and replaces adds a class to the ol and removes styling on li",
+    #     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument("SITE_ID", help="The SITE_ID on which to work")
+    # parser.add_argument('-d', '--debug', action='store_true')
+    # args = vars(parser.parse_args())
+    #
+    # APP['debug'] = APP['debug'] or args['debug']
 
-    APP['debug'] = APP['debug'] or args['debug']
-
-    run(args['SITE_ID'], APP)
+    # run(args['SITE_ID'], APP)
+    run('1', APP)
 
 
 if __name__ == '__main__':
