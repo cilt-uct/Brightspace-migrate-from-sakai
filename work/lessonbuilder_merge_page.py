5d4fabd25fb7c730852d93b6f21d5331b7b9f47c
NgoniChoga <nickchoga@gmail.com>
2023-08-31 23:17:36 +0100

    AMA-674 - reduce fragmentation by merging lessons

---
+++ work/lessonbuilder_merge_page.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import argparse
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from lib.utils import *
+
+
+def run(SITE_ID, APP):
+    logging.info('Merge lessons page: {}'.format(SITE_ID))
+    site_folder = APP['archive_folder']
+
+    xml_src = r'{}{}-archive/lessonbuilder.xml'.format(site_folder, SITE_ID)
+    xml_old = r'{}{}-archive/lessonbuilder.old'.format(site_folder, SITE_ID)
+    shutil.copyfile(xml_src, xml_old)
+
+    remove_unwanted_characters(xml_src)
+
+    file_path = os.path.join(site_folder, xml_src)
+
+    with open(file_path, "r", encoding="utf8") as fp:
+        merged = BeautifulSoup('<div></div>', 'html.parser')
+        soup = BeautifulSoup(fp, 'xml')
+        pages = soup.find_all('page')
+        for page in pages:
+            items = page.find_all('item')
+            for item in items:
+                if item.attrs['type'] == '5':
+                    html = BeautifulSoup(item.attrs['html'], 'html.parser')
+                else:
+                    #  Placeholder
+                    html = BeautifulSoup('<p>PLACEHOLDER</p>', 'html.parser')
+                merged.div.append(html)
+
+            updated_item = page.find('item', {'type': '5'})
+            updated_item['html'] = str(merged)
+            updated_item['data-merged'] = True
+
+            for item in items:
+                if not item.attrs.get('data-merged'):
+                    item.extract()
+
+        updated_xml = soup.prettify()
+        with open(file_path, 'w') as file:
+            file.write(updated_xml)
+
+
+def main():
+    global APP
+    parser = argparse.ArgumentParser(description="This script takes as input the 'lessonbuilder.xml' file inside "
+                                                 "the site-archive folder and merges page content",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("SITE_ID", help="The SITE_ID on which to work")
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = vars(parser.parse_args())
+
+    APP['debug'] = APP['debug'] or args['debug']
+
+    run(args['SITE_ID'], APP)
+
+
+if __name__ == '__main__':
+    main()
