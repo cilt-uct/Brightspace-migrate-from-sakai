da659985c7464ed8561ba48dbe8677576096e28e
Ngonidzashe Nicholas Choga <nickchoga@gmail.com>
2023-10-13 08:02:10 +0100

    AMA-716 - turn embedded folder list into static html (#38)
    * AMA-716 - turn embedded resources into static html
    
    * AMA-716 - do not split the rel-id
    
    * AMA-716 - make html well-formed
    
    * AMA-716 - rearrange workflow order
    
    * AMA-716 - update workflow to use merge items
    
    * AMA-716 - new lines
    
    * AMA-716 - add a check for site information

--- tests/test_lessonbuilder_merge_items.py
+++ tests/test_lessonbuilder_merge_items.py
@@ -1,53 +1,32 @@
+import argparse
 import os
 import shutil
 import unittest
-import argparse
 
 import config.config
-from work.lessonbuilder_merge_page import main
 from unittest.mock import patch
+from work.lessonbuilder_merge_items import main
 from bs4 import BeautifulSoup
 
-class MergePageTestCase(unittest.TestCase):
+
+class AddResourcesHtmlTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
         config.config.APP['archive_folder'] = self.ROOT_DIR + '/test_files/'
 
     @patch('argparse.ArgumentParser.parse_args',
-           return_value=argparse.Namespace(SITE_ID='site_merge', debug=False))
-    def test_main_multiple_questions(self, *_):
+           return_value=argparse.Namespace(SITE_ID='site_resource_html', debug=False))
+    def test_add_resource_html(self, _):
         main()
-        xml_src = self.ROOT_DIR + '/test_files/site_merge-archive/lessonbuilder.xml'
-        xml_old = self.ROOT_DIR + '/test_files/site_merge-archive/lessonbuilder.old'
-        self.check_converted(xml_src=xml_src)
-        shutil.move(xml_old, xml_src)
-
-
-    def check_converted(self, xml_src):
+        xml_src = self.ROOT_DIR + '/test_files/site_resource_html-archive/lessonbuilder.xml'
+        xml_old = self.ROOT_DIR + '/test_files/site_resource_html-archive/lessonbuilder.xml.orig'
         file_path = os.path.join(xml_src)
-        page_count = 0
-
         with open(file_path, "r", encoding="utf8") as fp:
             soup = BeautifulSoup(fp, 'xml')
-            pages = soup.find_all('page')
-            for page in pages:
-                items = page.find_all('item')
-
-                if page_count == 0:
-                    self.assertEqual(2, len(items))
-                    self.assertEqual("5", items[0].attrs['type'])
-                    self.assertEqual("7", items[1].attrs['type'])
-                if page_count == 1:
-                    self.assertEqual(2, len(items))
-                    self.assertEqual("7", items[0].attrs['type'])
-                    self.assertEqual("5", items[1].attrs['type'])
-                if page_count == 2:
-                    self.assertEqual(3, len(items))
-                    self.assertEqual("7", items[0].attrs['type'])
-                    self.assertEqual("5", items[1].attrs['type'])
-                    self.assertEqual("1", items[2].attrs['type'])
-                if page_count == 3:
-                    self.assertEqual(1, len(items))
-                    self.assertEqual("5", items[0].attrs['type'])
-
-                page_count += 1
+            page = soup.find('item', attrs={"type": "5"})
+            html = BeautifulSoup(page.get('html'), 'html.parser')
+            div = html.find('div', attrs={"data-type": "folder-list"})
+            self.assertIsNotNone(div)
+            paragraphs = div.find_all('a')
+            self.assertEqual(8, len(paragraphs))
+        shutil.move(xml_old, xml_src)
