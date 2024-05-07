01d6854459d1a9f43acbff45a35de33ff5f7dc5e
Ngonidzashe Nicholas Choga <nickchoga@gmail.com>
2024-05-07 21:00:54 +0100

    AMA-700 - update tests, simplify

--- tests/test_syllabus_process.py
+++ tests/test_syllabus_process.py
@@ -8,6 +8,7 @@ import config.config
 from work.syllabus_process import main
 from unittest.mock import patch
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 
 class SyllabusRejiggerTestCase(unittest.TestCase):
@@ -23,27 +24,28 @@ class SyllabusRejiggerTestCase(unittest.TestCase):
         main()
         xml_src = self.ROOT_DIR + '/test_files/syllabus-archive/syllabus.xml'
         xml_old = self.ROOT_DIR + '/test_files/syllabus-archive/syllabus.old'
-        try:
-            self.check_converted(xml_src=xml_src)
-        finally:
-            shutil.move(xml_old, xml_src)
 
-    def check_converted(self, xml_src: str):
-        tree = ET.parse(xml_src)
-        root = tree.getroot()
+        new_tree = ET.parse(xml_src)
+        new_root = new_tree.getroot()
 
-        # Find syllabus_data nodes
-        syllabus_data = root.findall(".//siteArchive/syllabus/syllabus_data/asset")
-        encoded_html = syllabus_data[0].get("syllabus_body-html")
+        old_tree = ET.parse(xml_old)
+        old_root = old_tree.getroot()
 
+        old_syllabus_data = old_root.findall(".//siteArchive/syllabus/syllabus_data")
+        new_syllabus_data = new_root.findall(".//siteArchive/syllabus/syllabus_data")
+        # old xml file has 3 syllabus_data
+        self.assertEqual(3, len(old_syllabus_data))
+        # new xml file has 1 syllabus_data, merged
+        self.assertEqual(1, len(new_syllabus_data))
+
+        encoded_html = new_root.findall(".//siteArchive/syllabus/syllabus_data/asset")[0].get("syllabus_body-html")
         after_decoded_html = base64.b64decode(encoded_html).decode("utf-8")
-        self.assertIsNotNone(after_decoded_html)
+        body_soup = BeautifulSoup(after_decoded_html, 'html.parser')
+        # check elements in the html file
+        self.assertEqual(1, len(body_soup.find_all('h1')))
+        self.assertEqual(3, len(body_soup.find_all('h2')))
 
-        with open(self.ROOT_DIR + '/test_files/syllabus-archive/new_syllabus.html', 'r') as file:
-            new_syllabus_html = file.read()
-
-        self.assertEqual(new_syllabus_html, after_decoded_html)
-
+        shutil.move(xml_old, xml_src)
 
 if __name__ == '__main__':
     unittest.main(failfast=True)
