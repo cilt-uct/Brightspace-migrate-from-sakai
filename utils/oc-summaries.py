7094e9af3250626bcce3a1c22a388f810c54aef3
Stephen Marquard <stephen.marquard@uct.ac.za>
2024-10-25 16:54:07 +0200

    OPENCAST-3233 Updates

--- utils/oc-summaries.py
+++ utils/oc-summaries.py
@@ -89,6 +89,12 @@ def update_topic(APP, org_id, topic_id, topic_html):
 
     return
 
+def add_topic(APP, org_id, topic_id, topic_html):
+
+    print("NOT IMPLEMENTED - add topic")
+
+    return
+
 def add_or_replace_topic(APP, org_id, module_name, topic_name, topic_html):
 
     content_toc = get_content_toc(APP, org_id)
@@ -108,7 +114,7 @@ def add_or_replace_topic(APP, org_id, module_name, topic_name, topic_html):
     if topic is None:
         # Create
         print("CREATE")
-        #add_topic(APP, org_id, module_id, topic_html)
+        add_topic(APP, org_id, module_id, topic_html)
     else:
         # Replace
         print("REPLACE")
@@ -122,34 +128,97 @@ def add_or_replace_topic(APP, org_id, module_name, topic_name, topic_html):
 
     return
 
-def create_summaries(APP, series_id, summary = True, mcq = False):
+def get_enriched_events(APP, oc_client, series_id):
 
     utc_zone = pytz.utc
     local_timezone =  pytz.timezone('Africa/Johannesburg')
 
-    ocAuth = getAuth('Opencast', ['username', 'password'])
-    if ocAuth['valid']:
-        oc_client = Opencast(APP['opencast']['base_url'], ocAuth['username'], ocAuth['password'])
-    else:
-        raise Exception('Opencast authentication required')
+    events = oc_client.get_events(series_id)
+
+    if events is None:
+        print(f"No events for series {series_id}")
+        return []
+
+    enriched_events = []
+    pub = 0
+
+    for event in events:
+
+        eventId = event['identifier']
+
+        # Start time in local timezone
+        event_start = event['start']
+        utc_start = datetime.strptime(event_start, "%Y-%m-%dT%H:%M:%SZ")
+        utc_start = utc_zone.localize(utc_start)
+        local_start = utc_start.astimezone(local_timezone)
+        local_start_str = local_start.strftime("%-d %b %Y %-H:%M")
+
+        event['local_start'] = local_start_str
+
+        if is_published(event):
+            # Published event
+            pub += 1
+
+            if has_captions(event):
+                # Get this from published json in due course
+                # https://cilt.atlassian.net/browse/OPENCAST-3254
+                asset = oc_client.get_asset(eventId)
+
+                # Extract the URL containing "nibity"
+                attachments = asset['mediapackage']['attachments']['attachment']
+                for attachment in attachments:
+                    if 'nibity' in attachment['@id']:
+                        nibity_url = attachment['url']
+                        json_from_zip = oc_client.get_asset_zip_contents(nibity_url, f"{eventId}.json")
+                        if not json_from_zip:
+                            logging.info(f"** {eventId} {local_start_str} attachment does not contain {eventId}.json")
+                            continue
+
+                        event_content = json.loads(json_from_zip)
+                        nid = list(event_content.keys())[0]
+                        event['enriched'] = event_content[nid]
+                        enriched_events.append(event)
+            else:
+                logging.info(f"** {eventId} {local_start_str} is published but has no captions")
+
+    logging.info(f"Series {series_id} has {len(events)} events, {pub} published events, {len(enriched_events)} published events with captions")
+
+    return enriched_events
+
+
+def create_summaries(APP, oc_client, series_id, summary = True, update = False):
 
     # Check the Amathuba site id from extended metadata
     series_metadata = oc_client.get_series_metadata(series_id, "ext/series")
+
+    if series_metadata is None:
+        raise Exception(f"Opencast series {series_id} not found (missing metadata)")
+
     for m_item in series_metadata:
         if m_item['id'] == "site-id":
             org_id = m_item['value']
 
+    # Valid org_id ?
+
+    amathuba = False
+
     # Check valid course
-    course_info = get_course_info(APP, org_id)
+    if len(org_id) <= 6:
+        course_info = get_course_info(APP, org_id)
+        logging.info(f"Series {series_id} is '{course_info['Name']}' https://amathuba.uct.ac.za/d2l/home/{org_id}")
+        amathuba = True
+    else:
+        logging.info(f"Series {series_id} is https://vula.uct.ac.za/portal/site/{org_id}")
 
-    logging.info(f"Generating summary page for {org_id} '{course_info['Name']}' from Opencast series {series_id}")
+    enriched_events = get_enriched_events(APP, oc_client, series_id)
 
-    # add_or_replace_topic(APP, org_id, "Lecture Recordings", "Lecture XSummaries", "<p/>")
+    if len(enriched_events) == 0:
+        return
 
-    events = oc_client.get_events(series_id)
-    if events is None:
-        print(f"No events for series {series_id}")
-        return None
+    if not amathuba:
+        return
+
+    ### Summary page
 
     # Start with an empty template
     template = "lecture-summary"
@@ -161,88 +230,52 @@ def create_summaries(APP, series_id, summary = True, mcq = False):
     if accordion is None:
         raise Exception("No Creator accordion found")
 
-    card_template = accordion.find("div", class_="card")
-
     card_index = 0
-
+    card_template = accordion.find("div", class_="card")
     event_model = None
 
-    for event in events:
+    for event in enriched_events:
 
-        if is_published(event) and has_captions(event):
+        event_model = event['enriched']['summary']['model']
 
-            # Published event
-            eventId = event['identifier']
-            #print(f"Published event with captions: {eventId} {event['start']} {event}")
+        eventId = event['identifier']
+        card_item = copy.copy(card_template)
 
-            # Get this from published json in due course
-            # https://cilt.atlassian.net/browse/OPENCAST-3254
-            asset = oc_client.get_asset(eventId)
+        summary_txt = event['enriched']['summary']['content'][0]['text']
+        #mcq_txt = event['enriched']['multiple_choice']['content'][0]['text']
+        kp_txt = event['enriched']['key_points']['content'][0]['text']
+        pq_txt = event['enriched']['practice_questions']['content'][0]['text']
+        # fr_txt = event['enriched']['further_reading']['content'][0]['text']
 
-            # Extract the URL containing "nibity"
-            attachments = asset['mediapackage']['attachments']['attachment']
-            for attachment in attachments:
-                if 'nibity' in attachment['@id']:
-                    nibity_url = attachment['url']
-                    # print("URL of the nibity file:", nibity_url)
-                    event_content = json.loads(oc_client.get_asset_zip_contents(nibity_url, f"{eventId}.json"))
+        #print(f"MCQ: {mcq_txt}")
+        #print(f"KP: {kp_txt}")
+        #print(f"PQ: {pq_txt}")
 
-                    nid = list(event_content.keys())[0]
+        # Presenter list
+        presenters = ', '.join(event['presenter'])
+        event_title = f"{event['local_start']} {event['title']}"
 
-                    event_model = event_content[nid]['summary']['model']
+        print(f"{eventId} {event_title}")
 
-                    # print(f"Event content: {event_content[nid]}")
+        # <h2 class="card-title" data-itemprop="0|0">Lecture 1 title</h2>
+        # <div class="card-body" data-itemprop="0|1"><p>Lecture 1 summary</p></div>
 
-                    event['enriched'] = event_content[nid]
+        # Append a new card
+        card_p = card_item.find("p")
+        card_p.string = f"Presenter(s): {presenters}"
 
-            if 'enriched' in event:
-                card_item = copy.copy(card_template)
+        card_title  = card_item.find("h2", class_="card-title")
+        card_title['data-itemprop']=f"{card_index}|0"
+        card_title.string = event_title
 
-                summary_txt = event['enriched']['summary']['content'][0]['text']
-                mcq_txt = event['enriched']['multiple_choice']['content'][0]['text']
-                kp_txt = event['enriched']['key_points']['content'][0]['text']
-                pq_txt = event['enriched']['practice_questions']['content'][0]['text']
-                # fr_txt = event['enriched']['further_reading']['content'][0]['text']
+        card_body = card_item.find("div", class_="card-body")
+        card_body['data-itemprop']=f"{card_index}|1"
+        card_body.append(BeautifulSoup(txt_to_html(summary_txt), "html.parser"))
+        card_body.append(BeautifulSoup(txt_to_html(kp_txt), "html.parser"))
+        card_body.append(BeautifulSoup(txt_to_html(pq_txt), "html.parser"))
 
-                if mcq:
-                    print(f"MCQ: {mcq_txt}")
-
-                #print(f"KP: {kp_txt}")
-                #print(f"PQ: {pq_txt}")
-
-                # Presenter list
-                presenters = ', '.join(event['presenter'])
-
-                # Start time in local timezone
-                event_start = event['start']
-                utc_start = datetime.strptime(event_start, "%Y-%m-%dT%H:%M:%SZ")
-                utc_start = utc_zone.localize(utc_start)
-                local_start = utc_start.astimezone(local_timezone)
-                local_start_str = local_start.strftime("%-d %b %Y %-H:%M")
-
-                event_title = f"{local_start_str} {event['title']}"
-
-                print(f"{eventId} {event_title}")
-
-                # <h2 class="card-title" data-itemprop="0|0">Lecture 1 title</h2>
-                # <div class="card-body" data-itemprop="0|1"><p>Lecture 1 summary</p></div>
-
-                # Append a new card
-                card_p = card_item.find("p")
-                card_p.string = f"Presenter(s): {presenters}"
-
-                card_title  = card_item.find("h2", class_="card-title")
-                card_title['data-itemprop']=f"{card_index}|0"
-                card_title.string = event_title
-
-                card_body = card_item.find("div", class_="card-body")
-                card_body['data-itemprop']=f"{card_index}|1"
-                card_body.append(BeautifulSoup(txt_to_html(summary_txt), "html.parser"))
-                card_body.append(BeautifulSoup(txt_to_html(kp_txt), "html.parser"))
-                card_body.append(BeautifulSoup(txt_to_html(pq_txt), "html.parser"))
-
-                accordion.append(card_item)
-                card_index += 1
+        accordion.append(card_item)
+        card_index += 1
 
     # Remove the template card and generate the new HTML
     card_template.decompose()
@@ -251,7 +284,8 @@ def create_summaries(APP, series_id, summary = True, mcq = False):
     logging.info(f"Model: {event_model}")
 
     # Create or update the topic page
-    add_or_replace_topic(APP, org_id, "Lecture Recordings", "Lecture Summaries", new_html)
+    if update:
+        add_or_replace_topic(APP, org_id, "Lecture Recordings", "Lecture Summaries", new_html)
 
     return
 
@@ -259,13 +293,17 @@ def main():
 
     APP = config.config.APP
 
+    logfile = f"{parent}/log/oc-summaries.log"
+    print(f"Logging to {logfile}")
+
     logger = logging.getLogger()
-    setup_logging(APP, logger, "summaries.log")
+    setup_logging(APP, logger, logfile)
 
     parser = argparse.ArgumentParser(description="This script gets Brightspace import statuses",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("SERIES_ID", help="The Opencast series on which to work")
+    parser.add_argument("--list", help="File containing a list of Opencast series IDs")
+    parser.add_argument("--series", help="The Opencast series ID on which to work")
 
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument('--mcq', action='store_true')
@@ -274,7 +312,23 @@ def main():
     if args['debug']:
         logger.setLevel(logging.DEBUG)
 
-    create_summaries(APP, args['SERIES_ID'], summary = True, mcq = args['mcq'])
+    series_id = args['series']
+    series_list = args['list']
+
+    ocAuth = getAuth('Opencast', ['username', 'password'])
+    if ocAuth['valid']:
+        oc_client = Opencast(APP['opencast']['base_url'], ocAuth['username'], ocAuth['password'])
+    else:
+        raise Exception('Opencast authentication required')
+
+    if series_id:
+        create_summaries(APP, oc_client, series_id, summary = True, update = False)
+
+    if series_list:
+        with open(series_list, "r") as list_file:
+            for series_id in list_file:
+                series_id = series_id.replace("\n","")
+                create_summaries(APP, oc_client, series_id, summary = True, update = False)
 
     logging.info("Done")
 
